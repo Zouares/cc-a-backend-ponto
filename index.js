dd3e6454e55bef39578924118655340fd1bd7b6d
Zouares <gabrielssoaress02@gmail.com>
2024-11-18 08:58:11 -0300

    commit

--- index.js
+++ index.js
@@ -1,27 +1,44 @@
 const express = require('express');
 const app = express();
 
-app.get('/users', (req,res) => {
-    res.send("Nesse rota vou retornar os usuarios");
+const sequelize = require('./config/db');
+const Usuario = require('./models/Usuario');
+const Ponto = require('./models/Ponto');
+
+
+sequelize.sync({ alter: true })
+    .then(() => {
+        console.log("SUCESSO!");
+    })
+    .catch(error => {
+        console.log(`Erro ao sincronizar as tabelas - ${error}`);
+    });
+
+
+Ponto.create({ tipo: 'I', dataHora: 'sfsdfsdfgsdfgsdfgdsfgss' });
+
+
+
+// app.METODO('rota/caminho', (req, res) => {})
+
+app.get('/users', (req, res) => {
+    res.send("Nessa rota vou retornar os usuários");
 });
 
-app.post('/users', (req,res) => {
-    res.send("Nessa rota users usando post");
-
+app.post('/users', (req, res) => {
+    res.send("Rota users usando post");
 });
 
-app.get('/user/:id1', (req,res) => {
+app.get('/user/:id', (req, res) => {
     res.send(`o parâmetro é ${req.params.id}`)
-
 });
 
-app.get('/user/:id1-id2', (req,res) => {
+app.post('/user/:id1-:id2', (req, res) => {
     res.send(req.params);
-
 });
 
-const port = 3000;
+const PORT = 3000;
 
-app.listen(port, () => {
-    console.log(`Servidor web ouvindo na porta ${port}!`);
+app.listen(PORT, () => {
+    console.log(`Servidor web ouvindo na porta ${PORT}`);
 });
